92819d52fc53813c3007098b7bfbb8fa5936c29a
Jesus <jfr1013@gmail.com>
2016-03-07 12:30:06 +0100

    Añadido constructor a la clase MiHashmap

---
+++ MiHashmap.java
@@ -0,0 +1,22 @@
+/**
+ * Write a description of class MiHashmap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashmap
+{
+    // Array para almacenar las claves
+    private String[] claves;
+    // Array para almacenar los valores
+    private int[] valores;
+    
+    /**
+     * Constructor for objects of class MiHashmap
+     */
+    public MiHashmap()
+    {
+        claves = new String[0];
+        valores = new int[0];
+    }
+}

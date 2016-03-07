5786ea8bb6e785be88d46a127d493858a3785b51
Jesus <jfr1013@gmail.com>
2016-03-07 13:21:25 +0100

    Añadido metodo put()

--- MiHashmap.java
+++ MiHashmap.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Write a description of class MiHashmap here.
  * 
@@ -10,7 +12,7 @@ public class MiHashmap
     private String[] claves;
     // Array para almacenar los valores
     private int[] valores;
-    
+
     /**
      * Constructor for objects of class MiHashmap
      */
@@ -19,4 +21,35 @@ public class MiHashmap
         claves = new String[0];
         valores = new int[0];
     }
+
+    /**
+     * Método para añadir claves y valores
+     */
+    public int put(String clave,int valor){
+        int indice = 0;
+        boolean repetida = false;
+        while (indice<valores.length && !repetida ) {
+            if (clave.equals(claves[indice])) {
+                repetida = true;
+                valores[indice] = valor;
+            }
+            indice++;
+        }
+        if (!repetida) {
+            indice = -1;
+            int cont = 0;
+            String[] tempClaves = new String[claves.length + 1];
+            int[] tempValores = new int[valores.length + 1];
+            for (cont=0;cont<claves.length;cont++) {
+                tempClaves[cont] = claves[cont];
+                tempValores[cont] = valores[cont];
+            }
+            tempClaves[tempClaves.length - 1] = clave;
+            tempValores[tempValores.length - 1] = valor;
+            claves = tempClaves;
+            valores = tempValores;
+        }
+        return indice;
+    }
 }
+
